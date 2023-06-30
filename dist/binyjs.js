689e782210b21b3e4c71d8255dbb68d70cc47c93
Neven DREAN <nevendrean@yahoo.fr>
2023-06-30 11:32:38 +0200

    tipo

--- dist/binyjs.js
+++ dist/binyjs.js
@@ -38,14 +38,14 @@ const A = (t) => Object.assign(Object.create(_), t), _ = {
     return this._val;
   },
   set val(t) {
-    const e = this, n = e.key ?? "id";
+    const e = this, s = e.key ?? "id";
     if (e._val = t, Array.isArray(t) && (!e.oldVal && (e.oldVal = /* @__PURE__ */ new Map()), e.target)) {
-      const { diff: c, action: i, curMap: l, swap: s } = k({
+      const { diff: c, action: i, curMap: l, swap: n } = w({
         newVector: t,
         curMap: new Map([...e.oldVal]),
-        key: n
+        key: s
       });
-      e.swap = s, e.oldVal = l, e._val = c, e.renderAction = i;
+      e.swap = n, e.oldVal = l, e._val = c, e.renderAction = i;
     }
     if (e.renderAction === "clear")
       return u({
@@ -58,7 +58,7 @@ const A = (t) => Object.assign(Object.create(_), t), _ = {
       key: e.key,
       swap: e.swap,
       response: e._val
-    }) : e.target && e.target.dispatchEvent(new Event("change"))), !Array.isArray(v) && (e.oldVal = v), e._val = v, e.renderAction = "";
+    }) : e.target && e.target.dispatchEvent(new Event("change"))), !Array.isArray(t) && (e.oldVal = t), e._val = t, e.renderAction = "";
   }
 }, f = {
   get actions() {
@@ -67,7 +67,7 @@ const A = (t) => Object.assign(Object.create(_), t), _ = {
   set actions(t) {
     this._actions = t;
   }
-}, w = (t) => f.actions = t;
+}, k = (t) => f.actions = t;
 function p(t) {
   for (let e of [...t.querySelectorAll("[data-change]")])
     e.onchange = f.actions[e.dataset.change];
@@ -80,39 +80,39 @@ function p(t) {
 }
 window.addEventListener("load", () => p(app));
 function h(t, e) {
-  const n = t.tagName;
+  const s = t.tagName;
   let c;
-  return n === "TBODY" ? c = new DOMParser().parseFromString(`<table><tbody>${e}</tbody></table>`, "text/html").querySelector("tbody") : c = new DOMParser().parseFromString(`<${n}>${e}</${n}>`, "text/html").querySelector(n), p(c), c.childNodes;
+  return s === "TBODY" ? c = new DOMParser().parseFromString(`<table><tbody>${e}</tbody></table>`, "text/html").querySelector("tbody") : c = new DOMParser().parseFromString(`<${s}>${e}</${s}>`, "text/html").querySelector(s), p(c), c.childNodes;
 }
-function u({ target: t, response: e, renderAction: n, key: c, swap: i }) {
+function u({ target: t, response: e, renderAction: s, key: c, swap: i }) {
   ({
     assign: () => (t.innerHTML = e.join(""), p(t)),
     append: () => {
-      const s = h(t, e.join(""));
-      t.append(...s);
+      const n = h(t, e.join(""));
+      t.append(...n);
     },
     clear: () => (t.innerHTML = "", p(document)),
     remove: () => {
-      const s = h(t, e.join(""));
-      for (let r of [...s]) {
+      const n = h(t, e.join(""));
+      for (let r of [...n]) {
         const o = Number(r.getAttribute("key"));
         t.querySelector(`[key="${o}"]`).remove();
       }
     },
     swap: () => {
-      const [s, r] = i;
-      t.insertBefore(t.children[s - 1], t.children[r - 1]), t.insertBefore(t.children[r - 1], t.children[s - 1]);
+      const [n, r] = i;
+      t.insertBefore(t.children[n - 1], t.children[r - 1]), t.insertBefore(t.children[r - 1], t.children[n - 1]);
     },
     update: function() {
-      const s = [...h(t, e.join(""))], r = s[0].getAttribute("key");
-      for (let o of s)
+      const n = [...h(t, e.join(""))], r = n[0].getAttribute("key");
+      for (let o of n)
         t.childNodes[o.getAttribute("key") - r].replaceWith(
           o
         );
     }
-  })[n]();
+  })[s]();
 }
-function k({ newVector: t, curMap: e, key: n }) {
+function w({ newVector: t, curMap: e, key: s }) {
   if (t.length === 0 && e.size > 1)
     return {
       action: "clear",
@@ -121,32 +121,32 @@ function k({ newVector: t, curMap: e, key: n }) {
       swap: []
     };
   {
-    const c = e.size, i = /* @__PURE__ */ new Map(), l = new Map([...e]), s = [];
+    const c = e.size, i = /* @__PURE__ */ new Map(), l = new Map([...e]), n = [];
     for (let a of t)
-      e.has(a) ? l.delete(a) : (e.set(a, a[n]), i.set(a, a[n]));
+      e.has(a) ? l.delete(a) : (e.set(a, a[s]), i.set(a, a[s]));
     if (l.size > 0)
       for (let a of l.keys())
         e.delete(a);
     if (i.size == 0) {
       let a = [...e.entries()].sort(
-        ([d, g], [y, b]) => d[n] < y[n] && -1
+        ([d, g], [y, b]) => d[s] < y[s] && -1
       );
       for (let [d, g] of [...v.entries()])
-        g[n] !== a[d][1] && (s.push(d + 1), e.set(a[d][0], g[n]));
+        g[s] !== a[d][1] && (n.push(d + 1), e.set(a[d][0], g[s]));
       a = [];
     }
     let r, o;
-    return l.size > 0 && i.size == 0 ? (r = "remove", o = [...l.keys()]) : s.length > 0 ? (r = "swap", o = [...i.keys()]) : l.size == 0 && i.size > 0 && c > 0 ? (r = "append", o = [...i.keys()]) : l.size == i.size && i.size < e.size ? (r = "update", o = [...i.keys()]) : (r = "assign", o = t), {
+    return l.size > 0 && i.size == 0 ? (r = "remove", o = [...l.keys()]) : n.length > 0 ? (r = "swap", o = [...i.keys()]) : l.size == 0 && i.size > 0 && c > 0 ? (r = "append", o = [...i.keys()]) : l.size == i.size && i.size < e.size ? (r = "update", o = [...i.keys()]) : (r = "assign", o = t), {
       action: r,
       diff: o,
       curMap: e,
-      swap: s.sort()
+      swap: n.sort()
     };
   }
 }
 const z = {
   state: A,
-  Actions: w
+  Actions: k
 };
 export {
   z as default
